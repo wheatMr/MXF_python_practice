f4b50f28941b665db9a4e8a8f79c152e11479ed3
liang <1354686920@qq.com>
2018-01-24 13:53:13 +0800

    测试 pycharm 上传git

---
+++ enroll.py
@@ -0,0 +1,12 @@
+# -*- coding: UTF-8 -*-
+# Time : 2018/1/24 13:32
+# Author : liang
+
+def enroll(name, age, city='xiaan'):
+    print('name:',name)
+    print('age:', age)
+    print('city:', city)
+
+enroll(city='beijing',name='liang',age=30)
+
+
